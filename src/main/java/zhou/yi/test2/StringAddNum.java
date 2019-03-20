c3917fb3ae530b98126abc99e301e8ca83405fbb
renmingpinming <renmingpinming@163.com>
2019-03-20 15:28:25 +0800

    System.out.println("5"+2);

---
+++ src/main/java/zhou/yi/test2/StringAddNum.java
@@ -0,0 +1,11 @@
+package zhou.yi.test2;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/3/20 15:20
+ */
+public class StringAddNum {
+    public static void main(String[] args){
+        System.out.println("5"+2);
+    }
+}
