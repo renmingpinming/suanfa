5f1095305685f03324c9a2c394f90d59cd63d1b7
renmingpinming <renmingpinming@163.com>
2019-05-06 18:58:18 +0800

    leetcode 561,566,581

---
+++ src/main/java/zhou/yi/leetCode/easy7/_561.java
@@ -0,0 +1,35 @@
+package zhou.yi.leetCode.easy7;
+
+import java.util.Arrays;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/5/6 8:46
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,4,3,2]
+ *
+ * 输出: 4
+ * 解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+ */
+public class _561 {
+    public int arrayPairSum(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int rst = 0;
+        for(int i = 0;i < nums.length;i=i+2){
+            rst+=nums[i];
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        _561 test = new _561();
+        int[] nums = new int[]{1,4,3,2};
+        System.out.println(test.arrayPairSum(nums));
+    }
+}
---
+++ src/main/java/zhou/yi/leetCode/easy7/_566.java
@@ -0,0 +1,65 @@
+package zhou.yi.leetCode.easy7;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/5/6 8:52
+ * 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+ *
+ * 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+ *
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+ *
+ * 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 1, c = 4
+ * 输出:
+ * [[1,2,3,4]]
+ * 解释:
+ * 行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+ * 示例 2:
+ *
+ * 输入:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 2, c = 4
+ * 输出:
+ * [[1,2],
+ *  [3,4]]
+ * 解释:
+ * 没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+ */
+public class _566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int totalNum = nums.length * nums[0].length;
+        if(totalNum != r * c || totalNum % r != 0){
+            return nums;
+        }
+        int[][] newNums = new int[r][c];
+        int newR = 0;
+        int newC = 0;
+        for(int i = 0;i< nums.length;i++){
+            for(int j = 0;j < nums[i].length;j++){
+                newNums[newR][newC] = nums[i][j];
+                newC++;
+                if (newC == c) {
+                    newC = 0;
+                    newR++;
+                }
+            }
+        }
+        return newNums;
+    }
+
+    public static void main(String[] args) {
+        _566 test = new _566();
+        int[][] nums = new int[][]{{1,2},{3,4}};
+        System.out.println(test.matrixReshape(nums,1,4));
+    }
+}
