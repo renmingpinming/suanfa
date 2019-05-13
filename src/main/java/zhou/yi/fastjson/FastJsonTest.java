5917a4379ab256ca9b70c9f19a3003a9a62127d8
renmingpinming <renmingpinming@163.com>
2019-05-13 09:00:07 +0800

    fastjson 转map、string

--- src/main/java/zhou/yi/fastjson/FastJsonTest.java
+++ src/main/java/zhou/yi/fastjson/FastJsonTest.java
@@ -3,7 +3,9 @@ package zhou.yi.fastjson;
 import com.alibaba.fastjson.JSON;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @Author: XiaoLang
@@ -20,6 +22,23 @@ public class FastJsonTest {
                 "  \"gmt_modify\": \"2017/10/12 22:00:53\"\n" +
                 "}";
         Model model = JSON.parseObject(str,Model.class);
-        System.out.println(model);
+//        System.out.println(model);
+
+        Model test = new Model();
+        Model test2 = new Model();
+        test.setNum(1);
+        test.setCompprodId(1);
+        test.setProditemId(1);
+        test2.setNum(1);
+        test2.setCompprodId(1);
+        test2.setProditemId(1);
+        List<Model> list = new ArrayList<>();
+        list.add(test);
+        list.add(test2);
+        String listToString = JSON.toJSONString(list);
+        System.out.println(listToString);
+
+        List<Model> stringToList = JSON.parseArray(listToString,Model.class);
+        System.out.println(stringToList);
     }
 }
