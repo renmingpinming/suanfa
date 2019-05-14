c91f69e1f23218edb36df9f59dce289e9312513f
renmingpinming <renmingpinming@163.com>
2019-05-14 18:26:39 +0800

    leetcode 234

--- src/main/java/zhou/yi/leetCode2/easy8/_234.java
+++ src/main/java/zhou/yi/leetCode2/easy8/_234.java
@@ -1,6 +1,7 @@
 package zhou.yi.leetCode2.easy8;
 
 import zhou.yi.leetCode.model.ListNode;
+import zhou.yi.leetCode.model.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,17 +9,55 @@ import java.util.List;
 /**
  * @Author: XiaoLang
  * @Date: 2019/5/13 18:25
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ *
+ * 输入: 1->2->2->1
+ * 输出: true
  */
 public class _234 {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> nums = new ArrayList<>();
-        while (head!=null){
-            nums.add(head.val);
+        if(head == null || head.next == null){
+            return Boolean.TRUE;
         }
-        int len = nums.size();
-        for (int i = 0;i<len/2;i++) {
-
+        //求中间节点
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //后面半个链表反转
+        ListNode pre = null;
+        ListNode now = slow.next;
+        ListNode temp = null;
+        while (now != null){
+            temp = now.next;
+            now.next = pre;
+            pre = now;
+            now = temp;
+        }
+        slow.next = pre;
+        slow = slow.next;
+        while (slow != null){
+            if(head.val != slow.val){
+                return Boolean.FALSE;
+            }
+            head = head.next;
+            slow = slow.next;
         }
         return Boolean.TRUE;
+    }
+
+    public static void main(String[] args) {
+        _234 test = new _234();
+        int[] nums = new int[]{1,2,2,1};
+        ListNode root = ListNode.createListNode(nums);
+        System.out.println(test.isPalindrome(root));
     }
 }
