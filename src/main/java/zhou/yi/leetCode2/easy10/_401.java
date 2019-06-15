ccccb2bfbf3e37531fee9f452e9b4adffd701af6
renmingpinming <renmingpinming@163.com>
2019-06-15 11:32:28 +0800

    leetcode 401,404

---
+++ src/main/java/zhou/yi/leetCode2/easy10/_401.java
@@ -0,0 +1,56 @@
+package zhou.yi.leetCode2.easy10;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/6/15 11:06
+ * 二进制手表顶部有 4 个 LED 代表小时（0-11），底部的 6 个 LED 代表分钟（0-59）。
+ *
+ * 每个 LED 代表一个 0 或 1，最低位在右侧。
+ * 例如，上面的二进制手表读取 “3:25”。
+ *
+ * 给定一个非负整数 n 代表当前 LED 亮着的数量，返回所有可能的时间。
+ *
+ * 案例:
+ *
+ * 输入: n = 1
+ * 返回: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-watch
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _401 {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new LinkedList<>();
+        //直接遍历  0:00 -> 12:00   每个时间有多少1
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == num)
+                    res.add(i + ":" + (j < 10 ? "0" + j : j));
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 计算二进制中1的个数
+     * @param n
+     * @return
+     */
+    int count1(int n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        _401 test = new _401();
+        System.out.println(test.readBinaryWatch(2));
+    }
+}
---
+++ src/main/java/zhou/yi/leetCode2/easy10/_404.java
@@ -0,0 +1,65 @@
+package zhou.yi.leetCode2.easy10;
+
+import zhou.yi.leetCode.model.TreeNode;
+
+import java.util.List;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/6/15 11:12
+ * 计算给定二叉树的所有左叶子之和。
+ *
+ * 示例：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null || root.left == null){
+            return 0;
+        }
+        if(root.left.left == null){
+            return root.left.val+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+        }else {
+            return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+        }
+    }
+
+    int res = 0;//存储结果
+
+    public int sumOfLeftLeaves2(TreeNode root) {
+        if(root==null)//root为空直接返回0
+            return res;
+        getSum(root);//递归函数,求得结果
+        return res;
+    }
+
+    public void getSum(TreeNode root) {
+        if (root.left != null) {//root.left不能为空
+            //root.left为左叶子
+            if (root.left.left == null && root.left.right == null)
+                res += root.left.val;//结果增加
+            else//否则继续递归
+                getSum(root.left);
+        }
+        if (root.right != null)//root.right不能为空
+            getSum(root.right);
+    }
+
+    public static void main(String[] args) {
+        _404 test = new _404();
+        int[] arrays = new int[]{0,2,4,1,0,3,-1,5,1,0,6,0,8};
+        List<TreeNode> treeNodes = TreeNode.createBinaryTree(arrays);
+        System.out.println(test.sumOfLeftLeaves(treeNodes.get(0)));
+    }
+}
