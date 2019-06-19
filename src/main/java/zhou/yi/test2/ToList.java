dd65c9a44b319a97af3d675efc6cb4d7f5904a30
renmingpinming <renmingpinming@163.com>
2019-06-19 18:25:26 +0800

    leetcode 6

---
+++ src/main/java/zhou/yi/test2/ToList.java
@@ -0,0 +1,16 @@
+package zhou.yi.test2;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/6/19 10:26
+ */
+public class ToList {
+    public static void main(String[] args) {
+        Long[] longOne = new Long[] {};
+        List<Long> r = Arrays.asList(longOne);
+        System.out.println(r);
+    }
+}
