dde5a7de73fea8849dd320aaf790e9fbaf19de4a
renmingpinming <renmingpinming@163.com>
2019-06-21 14:04:37 +0800

    test

---
+++ src/main/java/zhou/yi/test2/ReplaceEnter.java
@@ -0,0 +1,12 @@
+package zhou.yi.test2;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/6/20 10:03
+ */
+public class ReplaceEnter {
+    public static void main(String[] args) {
+        String s = "\n123544";
+        System.out.println(s.replace("\n",""));
+    }
+}
