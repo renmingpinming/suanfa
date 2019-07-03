88b87f5ae3f59072cb8b10982b3e7249800c335c
renmingpinming <renmingpinming@163.com>
2019-07-03 13:30:42 +0800

    leetcode 19

---
+++ src/main/java/zhou/yi/leetCode2/medium2/_19.java
@@ -0,0 +1,42 @@
+package zhou.yi.leetCode2.medium2;
+
+import zhou.yi.leetCode.model.ListNode;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/7/2 13:47
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ */
+public class _19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode ahead = dummy;
+        for (int i = 0;i <= n;i++){
+            ahead = ahead.next;
+        }
+        ListNode behind = dummy;
+        while (ahead !=null){
+            behind = behind.next;
+            ahead = ahead.next;
+        }
+        behind.next = behind.next.next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        _19 test = new _19();
+        int[] nums = new int[]{1,2,3,4,5};
+        ListNode head = ListNode.createListNode(nums);
+        test.removeNthFromEnd(head,2);
+    }
+}
--- src/main/java/zhou/yi/test2/ReplaceEnter.java
+++ src/main/java/zhou/yi/test2/ReplaceEnter.java
@@ -1,12 +1,17 @@
 package zhou.yi.test2;
 
+import java.util.List;
+
 /**
  * @Author: XiaoLang
  * @Date: 2019/6/20 10:03
  */
 public class ReplaceEnter {
     public static void main(String[] args) {
-        String s = "\n123544";
-        System.out.println(s.replace("\n",""));
+        String s = "\n1235 44,\n154  89";
+//        System.out.println(s.replace("\n",""));
+//        System.out.println(s.replace(" ",""));
+        String[] x = s.replace("\n","").replace(" ","").split(",");
+        System.out.println(x);
     }
 }
