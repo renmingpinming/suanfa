0722a382215953642d030df0d726fa36c3828eca
renmingpinming <renmingpinming@163.com>
2019-07-18 15:31:13 +0800

    map取空key

---
+++ src/main/java/zhou/yi/test3/MapTest.java
@@ -0,0 +1,17 @@
+package zhou.yi.test3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: XiaoLang
+ * @Date: 2019/7/18 15:20
+ */
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("a","b");
+        map.put("c","d");
+        System.out.println(map.get("d"));
+    }
+}
