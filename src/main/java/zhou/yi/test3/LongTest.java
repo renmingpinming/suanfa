5ee38012acdc37b674bf5ffd0942ddf387a096d7
renmingpinming <renmingpinming@163.com>
2019-08-27 17:09:49 +0800

    long test

---
+++ src/main/java/zhou/yi/test3/LongTest.java
@@ -0,0 +1,12 @@
+package zhou.yi.test3;
+
+/**
+ * @Author:XiaoLang
+ * @Date:2019-08-27 10:35
+ */
+public class LongTest {
+    public static void main(String[] args) {
+        Long l1 = new Long(1);
+        System.out.println(l1>=1L);
+    }
+}
