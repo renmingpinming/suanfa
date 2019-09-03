236ae8023a66fa802eb8ca6a68cec5c326ddbfb4
renmingpinming <renmingpinming@163.com>
2019-09-03 15:57:10 +0800

    list spliterator

--- src/main/java/zhou/yi/test2/ListTest.java
+++ src/main/java/zhou/yi/test2/ListTest.java
@@ -2,6 +2,7 @@ package zhou.yi.test2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
 
 /**
  * @Author: XiaoLang
@@ -23,5 +24,15 @@ public class ListTest {
         System.out.println(list1);
 
         list1.forEach(l-> System.out.println(l));
+
+        list1.add("d");
+        list1.add("e");
+        list1.add("f");
+        Spliterator<String> x = list1.spliterator();
+        System.out.println(x);
+        Spliterator<String> x1 = x.trySplit();
+        System.out.println(x1);
+        Spliterator<String> x2 = x.trySplit();
+        System.out.println(x2);
     }
 }
