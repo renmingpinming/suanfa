25991f84e75dbc1eda5c945136f3157cbc3d19af
renmingpinming <renmingpinming@163.com>
2019-09-06 16:41:41 +0800

    数字类型相加

---
+++ src/main/java/zhou/er/test/NumberTest.java
@@ -0,0 +1,16 @@
+package zhou.er.test;
+
+/**
+ * @Author:XiaoLang
+ * @Date:2019-09-06 16:38
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        Double d = new Double(12.3);
+        Float f = new Float(13);
+        System.out.println(d+1f);
+        System.out.println(d+1);
+        System.out.println(d+f);
+        System.out.println(f+1);
+    }
+}
