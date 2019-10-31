579bb299c8d72c33308dc173fc9994e25dfbbd40
renmingpinming <renmingpinming@163.com>
2019-10-31 16:29:10 +0800

    list 取bigdecimal字段最大值

--- src/main/java/zhou/er/test/BigDecimalTest.java
+++ src/main/java/zhou/er/test/BigDecimalTest.java
@@ -1,6 +1,12 @@
 package zhou.er.test;
 
+import zhou.er.model.PoBudgetDo;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @Author:XiaoLang
@@ -9,6 +15,21 @@ import java.math.BigDecimal;
 public class BigDecimalTest {
     public static void main(String[] args) {
         BigDecimal num = new BigDecimal(10).setScale(2,BigDecimal.ROUND_HALF_UP);
-        System.out.println(num);
+        List<PoBudgetDo> budgetDos = new ArrayList<>();
+        PoBudgetDo d1 = new PoBudgetDo();
+        PoBudgetDo d2 = new PoBudgetDo();
+        PoBudgetDo d3 = new PoBudgetDo();
+        PoBudgetDo d4 = new PoBudgetDo();
+        d1.setThreshold(new BigDecimal(5));
+        d2.setThreshold(new BigDecimal(3));
+        d3.setThreshold(new BigDecimal(7));
+        d4.setThreshold(new BigDecimal(9));
+        budgetDos.add(d1);
+        budgetDos.add(d2);
+        budgetDos.add(d3);
+        budgetDos.add(d4);
+        Optional<BigDecimal> max = budgetDos.stream().map(a -> a.getThreshold()).max(Comparator.naturalOrder());
+        BigDecimal max2 = budgetDos.stream().map(a -> a.getThreshold()).max(Comparator.naturalOrder()).orElse(BigDecimal.ZERO);
+        Optional<PoBudgetDo> max1 = budgetDos.stream().max((u1, u2) -> u1.getThreshold().compareTo(u2.getThreshold()));
     }
 }
