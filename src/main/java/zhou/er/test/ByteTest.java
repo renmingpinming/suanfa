d3d86aec36435a85fd335fdb3cb4c381e67436ed
renmingpinming <renmingpinming@163.com>
2020-01-07 16:35:06 +0800

    byte比较

---
+++ src/main/java/zhou/er/test/ByteTest.java
@@ -0,0 +1,16 @@
+package zhou.er.test;
+
+/**
+ * @Author:XiaoLang
+ * @Date:2020-01-07 16:27
+ */
+public class ByteTest {
+    public static void main(String[] args) {
+        Byte status = 1;
+        if(status.equals((byte)1)){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+    }
+}
